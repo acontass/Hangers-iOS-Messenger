50ef9c647f1e80c8755dfff53007babecfa4da54
Anthony CONTASSOT-VIVIER <acontass@student.42.fr>
2019-01-31 23:53:41 +0100

    Add node socket server

---
+++ server.js
@@ -0,0 +1,25 @@
+const net = require('net');
+const fs = require('fs');
+
+var server = net.createServer(function(socket) {
+	console.log('Client connected');
+
+	socket.on('data', function(data) {
+		console.log(data.toString());
+		fs.appendFile(`${__dirname}/chat.txt`, data.toString() + '\n', function(err) {
+    	if(err)
+        	return console.log(err);
+			fs.readFile(`${__dirname}/chat.txt`, 'utf8', function(err, contents) {
+				if (err)
+					return
+				socket.write(contents)
+			});
+		});
+	});
+
+	socket.on('error', function(error) {
+		console.log(error);
+	});
+});
+
+server.listen(4242, '127.0.0.1');
