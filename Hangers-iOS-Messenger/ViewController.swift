0efa344c878a088375e9239d7522283546e13750
Anthony CONTASSOT-VIVIER <acontass@student.42.fr>
2019-01-31 11:21:31 +0100

    Initial Commit

---
+++ Hangers-iOS-Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangers-iOS-Messenger
+//
+//  Created by Anthony CONTASSOT-VIVIER on 31/01/2019.
+//  Copyright © 2019 me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Hangers-iOS-MessengerTests/Hangers_iOS_MessengerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hangers_iOS_MessengerTests.swift
+//  Hangers-iOS-MessengerTests
+//
+//  Created by Anthony CONTASSOT-VIVIER on 31/01/2019.
+//  Copyright © 2019 me. All rights reserved.
+//
+
+import XCTest
+@testable import Hangers_iOS_Messenger
+
+class Hangers_iOS_MessengerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
